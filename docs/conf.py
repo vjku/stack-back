813bd5c019fd51553845083c0f797f791f66d3cf
lawndoc <lawndoc@users.noreply.github.com>
2025-01-11 04:00:05 +0000

    automated version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Zetta.IO Technology AS'
 author = 'Zetta.IO Technology AS'
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.2"
+release = "1.0.3"
 
 # -- General configuration ---------------------------------------------------
 
